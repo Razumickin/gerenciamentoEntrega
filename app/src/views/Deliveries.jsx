d872c8a5a1d5f54c23a9729197c90aa4b7ea058f
Razumickin <razumickin@hotmail.com>
2024-03-01 22:42:43 -0300

    Conclusão da visualização da entrega.

--- app/src/views/Deliveries.jsx
+++ app/src/views/Deliveries.jsx
@@ -1,4 +1,4 @@
-import {useEffect, useRef, useState} from "react";
+import {useEffect, useState} from "react";
 import axiosClient from "../axios-client.js";
 
 /**
@@ -19,7 +19,6 @@ export default function Deliveries(){
     const getDeliveries = () => {
         axiosClient.get('/deliveries')
             .then(({ data }) => {
-                console.log(data.data);
                 setDeliveries(data.data)
             }).catch(error => {
                 const response = error.response
@@ -28,37 +27,35 @@ export default function Deliveries(){
     }
 
     return(
-        <div>
-            <div className='row'>
-                <table className='table table-hover'>
-                    <thead>
-                    <tr>
-                        <th scope="col">Nome destinatario</th>
-                        <th scope="col">CPF destinatario</th>
-                        <th scope="col">Endereço de entrega</th>
-                        <th scope="col">Volumes</th>
-                        <th scope="col">Remetente</th>
-                        <th scope="col">Transportadora</th>
-                        <th scope="col">Ultima atualização</th>
-                        <th scope="col">Ações</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                        {deliveries.map(del => (
-                            <tr  key={del.entrega_id}>
-                                <td><span className='fs-6'>{del.destinatario.nome}</span></td>
-                                <td>{del.destinatario.cpf}</td>
-                                <td>{del.destinatario.endereco}, {del.destinatario.cep}, {del.destinatario.estado}, {del.destinatario.pais}</td>
-                                <td>{del.volumes}</td>
-                                <td>{del.remetente}</td>
-                                <td>{del.transportadora.fantasia}</td>
-                                <td>{del.ultimoRastreamento.mensagem}</td>
-                                <td className='align-middle'><Link to={'/delivery/' + del.entrega_id} className='btn btn-outline-dark btn-sm'><FontAwesomeIcon icon={faEye}/></Link></td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
+        <div className='row'>
+            <table className='table table-hover'>
+                <thead>
+                <tr>
+                    <th scope="col">Nome destinatario</th>
+                    <th scope="col">CPF destinatario</th>
+                    <th scope="col">Endereço de entrega</th>
+                    <th scope="col">Volumes</th>
+                    <th scope="col">Remetente</th>
+                    <th scope="col">Transportadora</th>
+                    <th scope="col">Ultima atualização</th>
+                    <th scope="col">Ações</th>
+                </tr>
+                </thead>
+                <tbody>
+                    {deliveries.map(del => (
+                        <tr  key={del.entrega_id}>
+                            <td><span className='fs-6'>{del.destinatario.nome}</span></td>
+                            <td>{del.destinatario.cpf}</td>
+                            <td>{del.destinatario.endereco}, {del.destinatario.cep}, {del.destinatario.estado}, {del.destinatario.pais}</td>
+                            <td>{del.volumes}</td>
+                            <td>{del.remetente}</td>
+                            <td>{del.transportadora.fantasia}</td>
+                            <td>{del.ultimoRastreamento.mensagem}</td>
+                            <td className='align-middle'><Link to={'/delivery/' + del.entrega_id} className='btn btn-outline-dark btn-sm'><FontAwesomeIcon icon={faEye}/></Link></td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     )
 }
--- app/src/views/Delivery.jsx
+++ app/src/views/Delivery.jsx
@@ -1,23 +1,107 @@
 import {useParams} from "react-router-dom";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import axiosClient from "../axios-client.js";
 
 export default function Delivery(){
+    const [transportadora, setTransportadora] = useState({
+        cnpj: '',
+        fantasia: '',
+        transportadora_id: ''
+    })
+    const [destinatario, setDestinario] = useState({
+        nome: '',
+        cpf: '',
+        endereco: '',
+        estado: '',
+        pais: '',
+        cep: ''
+    })
+    const [delivery, setDelivery] = useState({
+        entrega_id: '',
+        volumes: '',
+        remetente: ''
+    });
+
     let {id} = useParams();
 
     useEffect(() => {
+        getDelivery(id);
+    }, [])
+
+    const getDelivery = (id) => {
         axiosClient.get(`/delivery/${id}`)
             .then(({data}) => {
-                console.log(data)
+                console.log(data.data)
+                setDelivery(data.data)
+                setDestinario(data.data.destinatario)
+                setTransportadora(data.data.transportadora)
             }).catch(error => {
-            const response = error.response
-            console.log(response.data.errors);
-        })
-    }, [])
+                const response = error.response
+                console.log(response.data.errors);
+            })
+    }
 
     return(
         <div>
-            Informação da entrega
+            <div className='row'>
+                <h4>Detalhes da entrega</h4>
+                <hr className='border-3 mb-4'/>
+                <div className='row'>
+                    <div className='col-5'>
+                        <label className='form-label'>Identificação da entrega</label>
+                        <input className="form-control" type="text" value={delivery.entrega_id} readOnly/>
+                    </div>
+                    <div className='col-5'>
+                        <label className='form-label'>Remetente</label>
+                        <input className="form-control" type="text" value={delivery.remetente} readOnly/>
+                    </div>
+                    <div className='col-2'>
+                        <label className='form-label'>Volumes</label>
+                        <input className="form-control" type="text" value={delivery.volumes} readOnly/>
+                    </div>
+                </div>
+                <h4 className='mt-3'>Detalhes do destinario</h4>
+                <hr className='border-3 mb-4'/>
+                <div className='row'>
+                    <div className='col-6'>
+                        <label className='form-label'>Nome</label>
+                        <input className="form-control" type="text" value={destinatario.nome} readOnly/>
+                    </div>
+                    <div className='col-6'>
+                        <label className='form-label'>CPF</label>
+                        <input className="form-control" type="text" value={destinatario.cpf} readOnly/>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col-9'>
+                        <label className='form-label'>Endereço</label>
+                        <input className="form-control" type="text"
+                               value={destinatario.endereco + ', ' + destinatario.estado + ', ' + destinatario.pais}
+                               readOnly/>
+                    </div>
+                    <div className='col-3'>
+                        <label className='form-label'>CEP</label>
+                        <input className="form-control" type="text" value={destinatario.cep} readOnly/>
+                    </div>
+                </div>
+                <h4 className='mt-3'>Detalhes do transportadora</h4>
+                <hr className='border-3 mb-4'/>
+                <div className='row'>
+                    <div className='col-4'>
+                        <label className='form-label'>Identificação da transportadora</label>
+                        <input className="form-control" type="text" value={transportadora.transportadora_id} readOnly/>
+                    </div>
+                    <div className='col-4'>
+                        <label className='form-label'>CNPJ</label>
+                        <input className="form-control" type="text" value={transportadora.cnpj} readOnly/>
+                    </div>
+                    <div className='col-4'>
+                        <label className='form-label'>Nome fantasia</label>
+                        <input className="form-control" type="text" value={transportadora.fantasia} readOnly/>
+                    </div>
+                </div>
+            </div>
         </div>
+
     )
 }
